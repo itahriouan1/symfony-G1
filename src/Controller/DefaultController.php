5e601769dfe02893caab0cf4f138169fe5378438
itahriouan zakaria <zakariaitahriouan@gmail.com>
2023-04-03 23:05:50 +0000

    installation

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController {
+
+    public function indexAction() {
+
+        return new Response("Hello, world!");
+
+    }
+
+
+}
