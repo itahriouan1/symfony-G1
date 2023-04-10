a9bb2fe761fb98858cf2064fd017d6acf5087799
itahriouan zakaria <zakariaitahriouan@gmail.com>
2023-04-10 18:21:04 +0000

    seance 06/04

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,13 +2,34 @@
 
 namespace App\Controller;
 
+use stdClass;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController {
+class DefaultController extends AbstractController {
 
+    /**
+     * @Route("/", name="index")
+     * @return Response
+     */
     public function indexAction() {
 
-        return new Response("Hello, world!");
+        $personne = new stdClass();
+        $personne->Nom = 'ITAHRIOUAN';
+        $personne->Prenom = 'Zakaria';
+
+        $t=[[1,2,3],[4,5,6],[7,8,9]];
+        return $this->render('base.html.twig', ['p'=>$personne, 't'=>$t]);
+
+    }
+    /**
+     *@Route("/home{p}", name="home")
+     *@return Response
+     */
+    public function home($p){
+        $html = '<h1>Home</h1>';
+        return $this->render('home.html.twig',['p'=>$p, 'html'=>$html]);
 
     }
 
---
+++ src/Entity/Stage.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StageRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StageRepository::class)]
+class Stage
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $Titre = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $Description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->Titre;
+    }
+
+    public function setTitre(string $Titre): self
+    {
+        $this->Titre = $Titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(string $Description): self
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+}
